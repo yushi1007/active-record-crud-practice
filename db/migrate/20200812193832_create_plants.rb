aefa415df427c6e21ecae4481dd399e4dfbbd39f
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-17 19:23:57 -0500

    adds timestamp

--- db/migrate/20200812193832_create_plants.rb
+++ db/migrate/20200812193832_create_plants.rb
@@ -3,6 +3,7 @@ class CreatePlants < ActiveRecord::Migration[5.2]
     self.create_table(:plants) do |table_helper|
       table_helper.string :name
       table_helper.date :bought
+      table_helper.timestamp :created_on
     end
   end
 end
